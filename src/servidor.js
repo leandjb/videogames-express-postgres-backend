f85e04d7a737babe151f5cdedfaf0f25d3682f5e
Leandro Barros <leandjb@gmail.com>
2026-08-08 16:15:38 -0500

    feat: added PUT and DELETE endpoints

--- src/servidor.js
+++ src/servidor.js
@@ -2,7 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const {basicConnection, pool} = require('./database.js');
 require('dotenv').config();
-const {obtener_todos_los_videojuegos} = require ('./controladores/videojuegos-controlador.js')
+const {obtener_todos_los_videojuegos,
+	obtener_videojuego_por_id,
+	crear_videojuego,
+	modificar_videojuego,
+	eliminar_videojuego
+}= require ('./controladores/videojuegos-controlador.js')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,8 +23,12 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/api/videojuegos', obtener_todos_los_videojuegos)
 
+app.get('/api/videojuegos', obtener_todos_los_videojuegos)
+app.get('/api/videojuegos/:id', obtener_videojuego_por_id)
+app.post('/api/videojuegos', crear_videojuego)
+app.put('/api/videojuegos/:id', modificar_videojuego)
+app.delete('/api/videojuegos/:id', eliminar_videojuego)
 
 app.listen(port, () => {
   console.log(`videogames app listening on port: ${port}`);
